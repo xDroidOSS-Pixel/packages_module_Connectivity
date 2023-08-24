CalyxOS/platform_frameworks_base

NetworkDiagnostics: Use Cloudflare DNS instead of Google DNS
Change-Id: I1300cff1609ebe87a6bec58b65cc724920a091bc

services/core/java/com/android/server/connectivity/NetworkDiagnostics.java
@@ -97,9 +97,9 @@ import javax.net.ssl.SSLSocketFactory;
 public class NetworkDiagnostics {
     private static final String TAG = "NetworkDiagnostics";
 
-    private static final InetAddress TEST_DNS4 = NetworkUtils.numericToInetAddress("8.8.8.8");
+    private static final InetAddress TEST_DNS4 = NetworkUtils.numericToInetAddress("1.1.1.1");
     private static final InetAddress TEST_DNS6 = NetworkUtils.numericToInetAddress(
-            "2001:4860:4860::8888");
+            "2606:4700:4700::1111");
 
     // For brevity elsewhere.
     private static final long now() {

xDroidOSS-Pixel/packages_module_Connectivity

(cherry picked from commit 8649aebad397dc3544348f88fac51fc8e4ac7347)
Signed-off-by: chiru2000 <chiranth@m.ms.evolution-x.org>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

service/src/com/android/server/connectivity/NetworkDiagnostics.java
@@ -97,9 +97,9 @@ import javax.net.ssl.SSLSocketFactory;
 public class NetworkDiagnostics {
     private static final String TAG = "NetworkDiagnostics";
 
-    private static final InetAddress TEST_DNS4 = InetAddresses.parseNumericAddress("8.8.8.8");
+    private static final InetAddress TEST_DNS4 = InetAddresses.parseNumericAddress("1.1.1.1");
     private static final InetAddress TEST_DNS6 = InetAddresses.parseNumericAddress(
-            "2001:4860:4860::8888");
+            "2606:4700:4700::1111");
 
     // For brevity elsewhere.
     private static final long now() {